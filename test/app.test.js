12b312d9864cba4c8b01355dfe6fad7f1ea7a1ab
Tonnie Nyabuto <tonnieobuya@gmail.com>
2024-06-16 12:51:17 +0300

    Add calculator

--- test/app.test.js
+++ test/app.test.js
@@ -1,25 +1,34 @@
 var assert = require("assert");
 var calculator = require("../app/calculator");
 
-describe("Calculator", function() {
-  it("returns zero", function() {
+describe("Calculator", function () {
+  it("returns zero", function () {
     let result = calculator.calculate("0");
     return assert.equal(result, 0);
   });
 
-  it("calculates addition", function() {
+  it("calculates addition", function () {
     let result = calculator.calculate("+ 3 4");
     return assert.equal(result, 3 + 4);
   });
 
-  it("calculates subtraction and multiplication", function() {
+  it("calculates subtraction and multiplication", function () {
     let result = calculator.calculate("- 3 * 4 5");
-    return assert.equal(result, 3 - (4 * 5));  // -17
+    return assert.equal(result, 3 - 4 * 5); // -17
   });
 
-  it("calculates addition and multiplication", function() {
+  it("calculates addition and multiplication", function () {
     let result = calculator.calculate("* + 3 4 5");
-    return assert.equal(result, (3 + 4) * 5);  // 35
+    return assert.equal(result, (3 + 4) * 5); // 35
   });
-})
 
+  it("calculates division, subtraction and addition", function () {
+    let result = calculator.calculate("/ - 3 4 + 5 2");
+    return assert.equal(result, (3 - 4) / (5 + 2));
+  });
+
+  it("calculates division, subtraction, multiplication and addition", function () {
+    let result = calculator.calculate("/ - 3 * 4 6 + 5 2");
+    return assert.equal(result, (3 - 4 * 6) / (5 + 2));
+  });
+});
